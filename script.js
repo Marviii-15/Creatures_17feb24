815aa3e55ae8699fcb8e40e9e0810a64f84ad0d7
Diego Redondo <marviluzpaternina@gmail.com>
2024-03-16 17:40:08 -0400

    HOLA

--- script.js
+++ script.js
@@ -3,29 +3,40 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
-const numberOfEnemies = 100;
+const numberOfEnemies = 20;
 const enemiesArray = [];
 
-const enemyImage = new Image();
-enemyImage.src = 'img/enemy1.png'
+let gameFrame = 0;
 
 class Enemy {
   constructor(){
-    this.x = Math.random() * canvas.width;
-    this.y = Math.random() * canvas.height;
-    this.width = 100;
-    this.height= 100;
-    this.speed = Math.random() * 4 - 2;
-    this.spriteWidth = 293;
-    this.spriteHeight = 155;
+    this.Image = new Image();
+    this.Image.src = 'img/enemy3.png'; 
+    this.speed = Math.random() * 4 + 1 ;
+    this.spriteWidth = 218;
+    this.spriteHeight = 177;
+    this.width = this.spriteWidth / 2;
+    this.height= this.spriteHeight / 2;
+    this.x = Math.random() * (canvas.width - this.width);
+    this.y = Math.random() * (canvas.height - this.height);
+    this.frame = 0;
+    this.flapSpeed = Math.floor(Math.random() * 3 + 1);
+    this.angle = 0;
+    this.angleSpeed = Math.random() * 0.2;
+    this.curve = Math.random() * 7;
   }
   update(){
-    this.x+= this.speed;
-    this.y+= this.speed;
+    //this.x -= this.speed;
+    //this.y += this.curve * Math.sin(this.angle);
+    this.angle += this.angleSpeed;
+    if (this.x + this.width < 0) this.x = canvas.width;
+    // animate sprites 
+    if (gameFrame % this.flapSpeed === 0){
+      this.frame > 4 ? this.frame = 0 : this.frame++;
+    } 
   }
   draw(){
-    ctx.strokeRect(this.x, this.y, this.width, this.height);
-    ctx.drawImage(enemyImage, 0, 0, this.spriteWidth, this.spriteHeight,this.x, this.y, this.width, this.height);
+    ctx.drawImage(this.Image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight,this.x, this.y, this.width, this.height);
   }
 };
 
@@ -38,6 +49,7 @@ function animate(){
       enemy.update();
       enemy.draw();
   });
+  gameFrame++;
   requestAnimationFrame(animate);
 }
 animate();
